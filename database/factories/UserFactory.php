6093d5bde1d4207cbed15616401334b221784a70
Vuong Nguyen <vuong_nguyen@datahouse.com>
2025-02-06 16:14:05 +0700

    [] (Flagtick Group) Build Page Builder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory
  */
 class UserFactory extends Factory
 {
-    /**
+    /*
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -21,12 +21,12 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
+    /*
      * Indicate that the model's email address should be unverified.
      *
      * @return static
